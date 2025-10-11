46c38274bd563499e954ab2b527fb3daefe80fb9
PRATIBHA-PC\Admin <pratibha.teggi@awpl.com>
2025-10-11 17:45:18 +0530

    Reverted context

--- src/pages/AccountSummary/AccountSummaryContext.tsx
+++ src/pages/AccountSummary/AccountSummaryContext.tsx
@@ -1,21 +1,28 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 import AccountSummary from "./AccountSummary"
+import Login from "../Login";
+
 
 
 interface AccountSummaryContextType {
   showAcctDetails: boolean;
   setShowAcctDetails: (value: boolean) => void;
+  showRegister: boolean;
+  setshowRegister: (value: boolean) => void;
   // add other properties here
 }
 const defaultContextValue: AccountSummaryContextType = {
   showAcctDetails: false,
   setShowAcctDetails: () => {},
+   showRegister: false,
+  setshowRegister: () => {},
 };
 
 export const AccountsummaryContext = createContext<AccountSummaryContextType>(defaultContextValue);
   const[showAcctDetails,setShowAcctDetails] = useState(false);
-
-<AccountsummaryContext.Provider value={{showAcctDetails,setShowAcctDetails}}>
+   const[showRegister,setshowRegister] = useState(false);
+<AccountsummaryContext.Provider value={{showAcctDetails,setShowAcctDetails,showRegister,setshowRegister}}>
 <AccountSummary/>
+<Login/>
 </AccountsummaryContext.Provider>
 
